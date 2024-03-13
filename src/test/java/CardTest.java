a44f12499e5e06d3182925118136a6b07ea5a233
zhafee3 <zhafee3@gmail.com>
2024-03-12 23:22:51 -0500

    cases

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -7,11 +7,25 @@ public class CardTest {
         Card card = new Card("Hearts", 10);
         assertEquals("Hearts", card.getSuit());
     }
+    @Test
+    public void testGetSuitMultiCard() {
+        Card card = new Card("Hearts", 10);
+        Card card2 = new Card("Clubs", 2);
+        assertEquals("Hearts", card.getSuit());
+        assertEquals("Clubs", card2.getSuit());
+    }
 
     @Test
     public void testGetValue() {
         Card card = new Card("Diamonds", 7);
         assertEquals(7, card.getValue());
+    }
+    @Test
+    public void testGetValueMultiCards() {
+        Card card = new Card("Diamonds", 7);
+        Card card2 = new Card("Diamonds", 10);
+        assertEquals(7, card.getValue());
+        assertEquals(10, card2.getValue());
     }
 
     @Test
