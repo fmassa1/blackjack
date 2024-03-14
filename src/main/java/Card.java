2f43928ff7f35a340e70fdb327abeb1303ce059d
zhafee3 <zhafee3@gmail.com>
2024-03-14 12:56:11 -0500

    comments

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,12 +3,14 @@
 // Card.java
 // class for the cards
 public class Card {
+    //sets properties to card (suit,value)
     private String suit;
     private int value;
     public Card(String suit, int value) {
         this.suit = suit;
         this.value = value;
     }
+    //gets suit and val
     public String getSuit(){
         return this.suit;
     }
